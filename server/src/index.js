81059a8d33a35012b434ead19ed22e85e30c379e
samm-iee <2405681@kiit.ac.in>
2026-06-22 12:35:24 +0530

    Day 3

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,9 @@ const groupRoutes = require('./routes/group')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5174'
+}))
 app.use(express.json())
 
 app.use('/api/groups', groupRoutes)
